7f9a15635ab261ace50879d2b991777b07bd6da6
Hanbin-GW <hanbin.scpsl@gmail.com>
2024-09-22 22:32:02 +0900

    add description

--- src/unit3/test.java
+++ src/unit3/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         double x = 1.0 - 0.1 - 0.1 - 0.1 - 0.1 - 0.1;
         System.out.println(x == 0.5);
+        // 자바에서 실수를 계산할때 오차 발생
     }
 }

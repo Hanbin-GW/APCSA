94116734aedf55ea719a65b43c73698e228fd22a
Hanbin-GW <hanbin.scpsl@gmail.com>
2024-09-22 22:30:52 +0900

    add test.java

---
+++ src/unit3/test.java
@@ -0,0 +1,8 @@
+package unit3;
+
+public class test {
+    public static void main(String[] args) {
+        double x = 1.0 - 0.1 - 0.1 - 0.1 - 0.1 - 0.1;
+        System.out.println(x == 0.5);
+    }
+}

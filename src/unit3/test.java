0ab720936b7fc88c2356933532952b46df04eca3
Hanbin-GW <hanbin.scpsl@gmail.com>
2024-09-23 08:17:59 +0900

    Update test.java

--- src/unit3/test.java
+++ src/unit3/test.java
@@ -5,5 +5,8 @@ public class test {
         double x = 1.0 - 0.1 - 0.1 - 0.1 - 0.1 - 0.1;
         System.out.println(x == 0.5);
         // 자바에서 실수를 계산할때 오차 발생
+
+        int count = 10;
+        boolean newline = count % 10 == 0;
     }
 }

3ef42b3a15650a44c075dfa8032c9b74d2abf0dd
Hanbin-GW <hanbin.scpsl@gmail.com>
2024-10-23 13:48:39 +0900

    Unit 4.1.1 add

---
+++ src/Unit4/step1/ExtractDigits.java
@@ -0,0 +1,19 @@
+package Unit4.step1;
+public class ExtractDigits
+{
+    public static void main(String[] args)
+    {
+        extractDigits(2938724);
+
+    }
+
+    public static void extractDigits(int num)
+    {
+
+        while(num > 0)
+        {
+            System.out.println(num % 10);
+            num /= 10;
+        }
+    }
+}
